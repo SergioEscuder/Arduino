2eb14e7e88be0e3876829c273ad6778aa08210c2
SergioEscuder <Sergioescuder@gmail.com>
2014-08-31 14:11:45 +0300

    Create PrintIP.ino

---
+++ Useful-Code/PrintIP.ino
@@ -0,0 +1,10 @@
+void PrintIP(){
+// print your local IP address:
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print("."); 
+  }
+  Serial.println();
+}
